008b39e5e6a9e50d3486c28cffe0c74acc113cf8
jcarpio <jose.carpio@gmail.com>
2023-05-09 12:56:31 +0200

    Update investment.pl

--- investment.pl
+++ investment.pl
@@ -1,8 +1,5 @@
 /*
 
-
-Investment = [ CD1s , CD2s, CD3s ]
-
 CD1s is an array that represent investment in CD1 for 6 months
 
 
@@ -12,9 +9,9 @@ Investment = [ [NCD11, NCD12, NCD13, NCD14, NCD15, NCD16],
                [NCD31,     0,     0,     0,     0,     0] 
              ]
 			 
-Max NCD3 = 60
-Max NCD2 = 
-Max NCD1 = 
+Max NCD3 = 100
+Max NCD2 = 150
+Max NCD1 = 200
 
 Cash_by_month = [400000 - ( NCD11 * 2000 + NCD2 * 3000 + NCD3 * 5000) #>= 100000, 			 
 			   PREV_REMAIN + NCD11*2000*0.01 + NCD11*2000 - NCD12*2000 #>=100000,
@@ -28,3 +25,43 @@ Cash_by_month = [400000 - ( NCD11 * 2000 + NCD2 * 3000 + NCD3 * 5000) #>= 100000
 			 
 
 */
+
+:- use_module(library(clpfd)).
+
+total_invest_ammount(400000).
+
+invest_revenue_months(2000, 1, [1,1,1,1,1,1]).
+invest_revenue_months(3000, 4, [1,0,0,1,0,0]).
+invest_revenue_months(5000, 9, [1,0,0,0,0,0]).
+
+
+
+minimun_cash(100000).
+
+invest_ammounts(As) :- findall(I, invest_revenue_months(I, _Re, _M), As).
+
+invest_revenues(Rs) :- findall(R, invest_revenue_months(_I, R, _M), Rs).
+
+invest_months(Ms) :- findall(M, invest_revenue_months(_I, _Re, M), Ms).
+
+pair(A, B, A-B).
+
+solution(S):-
+  invest_months(Ms), same_length(Ms, S),
+  Ms = [M1|MTail],
+  maplist(same_length(M1), S),
+  append(S, Vs),
+  transpose(Ms, TMs).
+  % maplist(month_invest(As, Rs), TMs, R).
+  
+  
+% check_max_invest(Ms, As, NCDs, Max,).
+  
+% total_month_ammount(Ms, As, Total).
+
+total_month_ammount([],[], 0).
+total_month_ammount([1|Ms], [A|As], [NCD|NCDs], TT):- 
+  total_month_ammount(Ms, As, T),
+  TT #= T + NCD*As.
+total_month_ammount([0|Ms], [_|As], NCDs, T):- 
+  total_month_ammount(Ms, As, T).
